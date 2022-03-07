8e4ad1541998d5867bc52578191ec5f4806eb37a
Xie <Xie@MacBook-Pro.local>
2022-03-07 15:41:43 +0900

    Initial commit.

---
+++ src/human_assets/src/index.js
@@ -0,0 +1,19 @@
+import { human } from "../../declarations/human";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await human.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
